7c11c6523ebee9a75ffc4836034310e4a3b21294
Adam Matthews <ammatthews2004@gmail.com>
2025-01-04 14:16:10 +0000

    fixing type error in favourite\route.ts

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -9,7 +9,7 @@ interface IParams {
 
 export async function POST (
     request: Request,
-    {params}: {params: IParams}
+    {params}: {params: {listingId: string}}
 ) {
     const currentUser = await getCurrentUser();
 
