e79b7efd90660ee30a6a631b498d7403eeba07f8
Adam Matthews <ammatthews2004@gmail.com>
2025-01-06 11:50:13 +0000

    style changes

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-8">
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
           {listings.map((listing) => {
             return (
               <ListingCard
